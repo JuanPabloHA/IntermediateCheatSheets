40ad8d230dc02e22c18b878a1d403bf4a3258765
Juan Pablo <juan.p.herreraalvarez@student.uts.edu.au>
2019-09-09 13:32:08 +1000

    test

--- PythonCheatSheets.py
+++ PythonCheatSheets.py
@@ -1,1 +1,9 @@
 # Introductory Cheat Sheets for Python
+
+## Loading datasets
+
+## Basic EDA
+
+## Data wrangling and transformation
+
+## Data visualisation
--- RCheatSheets.R
+++ RCheatSheets.R
@@ -1,1 +1,16 @@
 # Introductory Cheat Sheets for R
+
+# Data types
+## vectors
+
+## Matrices
+
+matrix
+
+# Loading datasets
+
+## Basic EDA
+
+## Data wrangling and transformation
+
+## Data visualisation
